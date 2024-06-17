e806c7bb98053bad9ca6688b18125df306a489ba
BVenetta <seeun.park.2000@gmail.com>
2024-06-17 13:38:54 +0200

    cv try

--- examples/full_project_setup/catkin_ws/src/learning_machines/src/learning_machines/cv.py
+++ examples/full_project_setup/catkin_ws/src/learning_machines/src/learning_machines/cv.py
@@ -1,22 +1,54 @@
 import numpy as np
 import cv2
-
+from data_files import FIGRURES_DIR
+from robobo_interface import (
+    IRobobo,
+    Emotion,
+    LedId,
+    LedColor,
+    SoundEmotion,
+    SimulationRobobo,
+    HardwareRobobo,
+)
 from util import get_limits
 
-cap = cv2.VideoCapture(2)
+def get_processed_image_front(self, ):
 
-green = [0,255,0] 
+    frame = self.get_image_front()
 
-while True:
-    ret, frame = cap.read()
-
+    green = [0, 255, 0]  
+    
     hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    lowerLimit, upperLimit = get_limits(color = green)
-
+    
+    lowerLimit, upperLimit = get_limits(color=green)
     mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
 
-    cv2.imshow('frame', mask)
+    return frame, mask  
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+while True:
+    
+    frame, mask = get_processed_image_front()
+    
+    if frame is not None:
+        cv2.imshow('frame', mask)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+# cap = cv2.VideoCapture(2)
+
+# green = [0,255,0] 
+
+# while True:
+#     ret, frame = cap.read()
+
+#     hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+#     lowerLimit, upperLimit = get_limits(color = green)
+
+#     mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+#     cv2.imshow('frame', mask)
+
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
